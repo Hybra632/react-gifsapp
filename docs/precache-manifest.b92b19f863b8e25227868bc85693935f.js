b0f139c9d066f0de96d8550355a25c173c78a172
Froylan <pinox666@gmail.com>
2020-09-01 12:31:03 -0500

    Docs agregado

---
+++ docs/precache-manifest.b92b19f863b8e25227868bc85693935f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6781888b34bb5fb64c5e36fd2706820e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f9b514c62b9ad5aef72",
+    "url": "/static/css/main.e129c56c.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9b514c62b9ad5aef72",
+    "url": "/static/js/main.73028292.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
